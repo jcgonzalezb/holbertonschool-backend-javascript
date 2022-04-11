e6902609d98a0046e07d67516e35c65d616adeb6
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-11 12:27:45 -0500

    syntax fixed

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,17 +2,17 @@
 // Modify function to instantiate variables using const and let.
 
 export function taskFirst() {
-	const task = 'I prefer const when I can.';
-	return task;
-      }
-      
-      export function getLast() {
-	return ' is okay';
-      }
-      
-      export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
-      
-	return combination;
-      }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
