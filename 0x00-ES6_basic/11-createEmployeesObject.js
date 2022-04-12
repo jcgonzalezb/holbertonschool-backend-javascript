139c5666ecdf38ad697b7866c265f876df856962
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-11 22:57:08 -0500

    object fixed

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,11 @@
 export default function createEmployeesObject(departmentName, employees) {
+  const val = [];
+  for (const i of employees) {
+    val.push(i);
+  }
+  const obj = {
+    [`${departmentName}`]: val,
+  };
 
+  return obj;
 }
