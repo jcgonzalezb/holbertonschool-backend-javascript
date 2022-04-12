a2fe38686a4a6fd6955c1edafcf023259e1946e2
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-11 21:57:41 -0500

    return fixed

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const list = [];
   for (const value of array) {
-    list.push(`${appendString}${value}`);
+    list.push(appendString + value);
   }
 
-  return array;
+  return list;
 }
