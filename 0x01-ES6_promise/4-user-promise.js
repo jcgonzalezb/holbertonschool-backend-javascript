390e0f2e5d4f5809ebf1b4819e5c96f3bd13028c
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-13 13:52:54 -0500

    if fixed

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,2 +1,12 @@
-wfsfsd
-
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    if (firstName && lastName) {
+      resolve({
+        firstName,
+        lastName,
+      });
+    } else {
+      reject (new Error())
+    }
+  });
+}
