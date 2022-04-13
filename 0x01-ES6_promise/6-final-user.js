2cc1c15bcfbda83c297e0fdca74cb48525d07817
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-13 17:03:13 -0500

    Promise.all fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,6 @@
+/* eslint-disable */
 import { signUpUser } from "./4-user-promise.js";
 import { uploadPhoto } from "./5-photo-reject.js";
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser, uploadPhoto]).then(result => {
-    const firstName = result[0].firstName;
-    const lastName = result[1].lastName;
-    const fileName = result[2].fileName;
-    console.log(result);
-  }).catch(error => {
-    console.log('');
-  });
+  return Promise.all([signUpUser, uploadPhoto])
 }
