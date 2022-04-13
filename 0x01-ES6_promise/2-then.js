708dacdb7656be63c93b60272b5da00053526c76
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-12 22:32:46 -0500

    finally fixed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
-        status: 200,
-        body: success,
-      });
-    } else {
-      reject(new Error(''));
-    }
-  });
+  return promise.then(() => (
+    { status : 200, body : 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
