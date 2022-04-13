e7a5164ae7319f7714193eada7e22b595ee27838
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-13 17:32:49 -0500

    async added

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 import { signUpUser } from "./4-user-promise.js";
 import { uploadPhoto } from "./5-photo-reject.js";
-export default function handleProfileSignup(firstName, lastName, fileName) {
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const signedUser = await signUpUser(firstName, lastName).then((data) => ({
     status: 'fulfilled',
     value: data,
