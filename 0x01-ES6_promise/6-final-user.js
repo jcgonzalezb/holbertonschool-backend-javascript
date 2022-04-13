750f6b3208b743a234abf5a6789dd4d396267e6b
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-13 17:23:08 -0500

    functions fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,15 @@
 import { signUpUser } from "./4-user-promise.js";
 import { uploadPhoto } from "./5-photo-reject.js";
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser, uploadPhoto])
+  const signedUser = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const uploadedPhoto = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+
+  return [signedUser, uploadedPhoto];
 }
