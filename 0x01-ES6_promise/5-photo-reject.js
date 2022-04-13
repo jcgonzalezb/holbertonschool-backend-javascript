28b1ae1ab9d35452bad511e64811e3a6cc52eaee
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-13 14:10:20 -0500

    initial version

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,11 @@
 export default function uploadPhoto(filename) {
-
+  return new Promise((resolve, reject) => {
+    if (filename) {
+      resolve({
+        filename
+      });
+    } else {
+      reject(new Error(`${fileName} cannot be processed`))
+    }
+  });
 }
