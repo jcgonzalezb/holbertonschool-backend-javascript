0eab0258e32087acdd0ed3cf81faae21e6f793d5
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-13 16:47:56 -0500

    Promise.all fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,12 @@
-handleProfileSignup
+import { signUpUser } from "./4-user-promise.js";
+import { uploadPhoto } from "./5-photo-reject.js";
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser, uploadPhoto]).then(result => {
+    const firstName = result[0].firstName;
+    const lastName = result[1].lastName;
+    const fileName = result[2].fileName;
+    console.log(result);
+  }).catch(error => {
+    console.log('');
+  });
+}
