4d6af5f08cc351b7d1d7fdf932e15de7a47553d5
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-18 11:41:42 -0500

    initial version

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default function getListStudents(id, firstName, location) {
+  const array =  [
+    {
+      "id": "1",
+      "firstName": "Guillaume",
+      "location": "San Francisco"
+    },
+    {
+      "id": "2",
+      "firstName": "James",
+      "location": "Columbia"
+    },
+    {
+      "id": "3",
+      "firstName": "Serena",
+      "location": "San Francisco"
+    }
+  ];
+}
