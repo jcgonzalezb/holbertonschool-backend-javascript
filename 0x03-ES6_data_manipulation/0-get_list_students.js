0f9749f88039c5daf52621892cae11e0326c4441
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-18 11:44:39 -0500

    syntax fixed

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,19 +2,19 @@
 export default function getListStudents(id, firstName, location) {
   const array =  [
     {
-      "id": "1",
-      "firstName": "Guillaume",
-      "location": "San Francisco"
+      'id': '1',
+      'firstName': 'Guillaume',
+      'location': 'San Francisco'
     },
     {
-      "id": "2",
-      "firstName": "James",
-      "location": "Columbia"
+      'id': '2',
+      'firstName': 'James',
+      'location': 'Columbia'
     },
     {
-      "id": "3",
-      "firstName": "Serena",
-      "location": "San Francisco"
+      'id': '3',
+      'firstName': 'Serena',
+      'location': 'San Francisco'
     }
   ];
 }
