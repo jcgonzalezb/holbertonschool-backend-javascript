53a142185a8629469818ea5960df6989a72730dd
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-19 17:37:12 -0500

    has method implemented

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function hasValuesFromArray(Set, arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (!Set.has(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+}
