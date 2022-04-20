f6703e6f05275fad5d4251f0baefa5c78fdb9765
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-20 10:53:47 -0500

    initial version

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,2 +1,27 @@
-adsada
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+class UserAccount {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+
+  constructor(firstName: string, lastName: string, age: number, location: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location =  location;
+  }
+}
+
+const user1: User = new UserAccount("Alex", "Murphy", 30, "Detroit");
+const user2: User = new UserAccount("John", "Constantine", 32, "Los Angeles");
+
+let values: any[] = [user1, user2];
+
 
