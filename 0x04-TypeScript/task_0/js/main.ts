6fe9fec78ba0da281fe3a3b71c8b5e7f74c2b46a
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-20 18:21:39 -0500

    main file

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -22,6 +22,31 @@ class UserAccount {
 const user1: User = new UserAccount("Alex", "Murphy", 30, "Detroit");
 const user2: User = new UserAccount("John", "Constantine", 32, "Los Angeles");
 
-let values: any[] = [user1, user2];
+const list: any[] = [user1, user2];
 
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
 
+table.style.background = "lightgray";
+table.appendChild(tbody);
+
+list.forEach((student: any): void => {
+    const row = document.createElement('tr');
+
+    const nameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+
+    nameCell.style.border = "2px solid white";
+    locationCell.style.border = "2px solid white";
+    nameCell.style.padding = "4px";
+    locationCell.style.padding = "4px";
+
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+
+document.body.appendChild(table);
