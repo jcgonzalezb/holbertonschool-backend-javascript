5e60a94567714bac0ae0b16d211ab3ee5cad17c4
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-07 11:44:20 -0500

    syntax fixed

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const results = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
     const lines = results;
