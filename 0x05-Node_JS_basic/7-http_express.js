2f6983846aaef111b66333d49cd8e28440af3402
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-07 11:39:27 -0500

    app.get fixed

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const students = require('./3-read_file_async');
+
 const app = express();
 const port = 1245;
-const host = '127.0.0.1';
-
-
+const hostname = '127.0.0.1';
 
 app.get('/', (req, res) => {
   res.statusCode = 200;
